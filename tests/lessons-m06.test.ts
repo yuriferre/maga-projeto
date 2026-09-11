14e13fc589a4d685cdd3c0b60ebeaaad44534a92
yuriferreiradevops <yuriferre@gmail.com>
2026-09-11 18:59:55 -0300

    content(m06): aula M06-02 commit messages e títulos de PR

--- tests/lessons-m06.test.ts
+++ tests/lessons-m06.test.ts
@@ -18,4 +18,13 @@ describe("M06 lessons", () => {
     maxSeconds: 45,
     prerequisites: ["M03-05", "M04-05"],
   });
+  checkLesson(bundle, "M06-02", {
+    vocabMin: 12,
+    dialogueMin: 6,
+    listening: 6,
+    quiz: 8,
+    cardsMin: 10,
+    maxSeconds: 45,
+    prerequisites: ["M06-01"],
+  });
 });
