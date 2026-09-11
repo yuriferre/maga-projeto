334d0f12fb64cf7e2772deded9296c6487ef9d5f
yuriferreiradevops <yuriferre@gmail.com>
2026-09-11 18:40:43 -0300

    content(m05): aula M05-03 verbos e preposições de infra

--- tests/lessons-m05.test.ts
+++ tests/lessons-m05.test.ts
@@ -27,4 +27,13 @@ describe("M05 lessons", () => {
     maxSeconds: 45,
     prerequisites: ["M05-01"],
   });
+  checkLesson(bundle, "M05-03", {
+    vocabMin: 12,
+    dialogueMin: 6,
+    listening: 6,
+    quiz: 8,
+    cardsMin: 10,
+    maxSeconds: 45,
+    prerequisites: ["M05-02"],
+  });
 });
