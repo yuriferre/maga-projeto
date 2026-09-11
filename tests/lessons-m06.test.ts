bcf28402b8dc1dca8f07615e1bb4c22635012f48
yuriferreiradevops <yuriferre@gmail.com>
2026-09-11 19:05:28 -0300

    content(m06): aula M06-04 comentários e respostas no PR

--- tests/lessons-m06.test.ts
+++ tests/lessons-m06.test.ts
@@ -36,4 +36,13 @@ describe("M06 lessons", () => {
     maxSeconds: 45,
     prerequisites: ["M06-02"],
   });
+  checkLesson(bundle, "M06-04", {
+    vocabMin: 12,
+    dialogueMin: 6,
+    listening: 6,
+    quiz: 8,
+    cardsMin: 10,
+    maxSeconds: 45,
+    prerequisites: ["M06-03"],
+  });
 });
